263a5d6e562a8fb1c04461a66a78611359d376d3
rl1987 <rl1987@sdf.lonestar.org>
2018-02-19 21:08:51 +0100

    Simplify hostname validation code

--- src/common/util.c
+++ src/common/util.c
@@ -1114,6 +1114,9 @@ string_is_valid_hostname(const char *string)
   if (!string || strlen(string) == 0)
     return 0;
 
+  if (string_is_valid_ipv4_address(string))
+    return 0;
+
   components = smartlist_new();
 
   smartlist_split_string(components,string,".",0,0);
@@ -1134,17 +1137,10 @@ string_is_valid_hostname(const char *string)
       break;
     }
 
-    if (c_sl_idx == c_sl_len - 1) {
-      do {
-        result = TOR_ISALPHA(*c);
-        c++;
-      } while (result && *c);
-    } else {
-      do {
-        result = (TOR_ISALNUM(*c) || (*c == '-') || (*c == '_'));
-        c++;
-      } while (result > 0 && *c);
-    }
+    do {
+      result = (TOR_ISALNUM(*c) || (*c == '-') || (*c == '_'));
+      c++;
+    } while (result > 0 && *c);
 
     if (result == 0) {
       break;
