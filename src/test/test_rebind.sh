677804465be5e0fb509c5fde53bbfe8282bc4cdf
rl1987 <rl1987@sdf.lonestar.org>
2018-08-05 16:59:47 +0300

    Try fixing Appveyor windows build

--- src/test/test_rebind.sh
+++ src/test/test_rebind.sh
@@ -4,6 +4,12 @@ set -x
 
 exitcode=0
 
-"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/test_rebind.py" "${abs_top_srcdir:-.}/src/app/tor" || exitcode=1
+
+if [ "$TESTING_TOR_BINARY" = "" ] ; then
+  echo "TESTING_TOR_BINARY not set"
+  exit 1
+fi
+
+"${PYTHON:-python}" "${abs_top_srcdir:-.}/src/test/test_rebind.py" "${TESTING_TOR_BINARY}" || exitcode=1
 
 exit ${exitcode}
