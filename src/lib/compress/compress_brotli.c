9ac55f99be4d138733b3d4959c014731aa655e3e
rl1987 <rl1987@sdf.lonestar.org>
2020-02-21 20:04:35 +0800

    Try initializing Brotli encoder instance

--- src/lib/compress/compress_brotli.c
+++ src/lib/compress/compress_brotli.c
@@ -1,5 +1,8 @@
 #include "orconfig.h"
 
+#include "lib/log/log.h"
+#include "lib/log/util_bug.h"
+#include "lib/malloc/malloc.h"
 #include "lib/compress/compress.h"
 #include "lib/compress/compress_brotli.h"
 #include "lib/string/printf.h"
@@ -48,19 +51,85 @@ tor_brotli_get_header_version_str(void)
 }
 
 struct tor_brotli_compress_state_t {
+  union {
+#ifdef HAVE_LIBBROTLIDEC
+    BrotliDecoderState *decoder_state;
+#endif
+#ifdef HAVE_LIBBROTLIENC
+    BrotliEncoderState *encoder_state;
+#endif
+  } u;
 
-  int is_encoding;
+  int compress;
 };
+
+static void *
+tor_brotli_alloc(void *opaque, size_t len)
+{
+  (void)opaque;
+  // XXX: use memarea?
+  return tor_malloc(len);
+}
+
+static void
+tor_brotli_free(void *opaque, void *address)
+{
+  (void)opaque;
+
+  tor_free_(address);
+}
+
+static uint32_t
+tor_brotli_quality_from_compression_level(
+    compression_level_t compression_level)
+{
+  switch (compression_level) {
+    default:
+    case BEST_COMPRESSION:
+      return BROTLI_MAX_QUALITY;
+    case HIGH_COMPRESSION:
+      return BROTLI_MAX_QUALITY - 1;
+    case MEDIUM_COMPRESSION:
+      return (BROTLI_MAX_QUALITY + BROTLI_MIN_QUALITY) / 2;
+    case LOW_COMPRESSION:
+      return BROTLI_MIN_QUALITY + 1;
+  }
+}
 
 tor_brotli_compress_state_t *
 tor_brotli_compress_new(int compress,
                       compress_method_t method,
                       compression_level_t compression_level)
 {
-  (void)compress;
-  (void)method;
-  (void)compression_level;
+  tor_assert(method == BROTLI_METHOD);
 
+  if (compress) {
+#ifdef HAVE_LIBBROTLIENC
+    tor_brotli_compress_state_t *result =
+      tor_malloc_zero(sizeof(tor_brotli_compress_state_t));
+
+    BrotliEncoderState *encoder_state = BrotliEncoderCreateInstance(
+        tor_brotli_alloc, tor_brotli_free, NULL);
+
+    if (!encoder_state) {
+      log_warn(LD_GENERAL, "Error creating Brotli encoder instance");
+      goto err;
+    }
+
+    BrotliEncoderSetParameter(encoder_state, BROTLI_PARAM_MODE,
+        BROTLI_MODE_TEXT);
+    BrotliEncoderSetParameter(encoder_state, BROTLI_PARAM_QUALITY,
+        tor_brotli_quality_from_compression_level(compression_level));
+
+    result->u.encoder_state = encoder_state;
+#endif
+  } else {
+#ifdef HAVE_LIBBROTLIDEC
+
+#endif
+  }
+
+ err:
   return NULL;
 }
 
